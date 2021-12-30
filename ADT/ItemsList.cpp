8eb213b47d543bebd58e1eb7a35a8cf9b7647e70
trinhduc9 <89338794+trinhduc9@users.noreply.github.com>
2021-12-30 06:06:43 -0500

    Getters/Setters

--- ADT/ItemsList.cpp
+++ ADT/ItemsList.cpp
@@ -35,7 +35,7 @@ int ItemsList::to_int(const string& item_id) {
 
 void ItemsList::showList(Node_items_collection *node){
     cout << node->node_itc_id << ' ';
-    cout << node->node_itc_name << ' ';
+    cout << node->node_itc_title << ' ';
     cout << node->node_itc_types << ' ';
     cout << node->node_itc_loans_types << ' ';
     cout << node->node_itc_copies << ' ';
@@ -56,7 +56,7 @@ void ItemsList::PrintListItems(ItemsList l) {
 
 void ItemsList::showNode(Node_items_collection* node) {
     cout << node->node_itc_id << ' ';
-    cout << node->node_itc_name << ' ';
+    cout << node->node_itc_title << ' ';
     cout << node->node_itc_types << ' ';
     cout << node->node_itc_loans_types << ' ';
     cout << node->node_itc_copies << ' ';
@@ -79,3 +79,84 @@ Node_items_collection *ItemsList::SearchItem(ItemsList list, const string& item_
     return nullptr;
 }
 
+Node_items_collection::Node_items_collection() {
+    this->node_itc_id = "I000 - 0000";
+    this->node_itc_title = "Item Title";
+    this->node_itc_types = "Item Type";
+    this->node_itc_loans_types = "Item Loan Type";
+    this->node_itc_copies = "Item Copies";
+    this->node_itc_fees = "0.99";
+    this->node_itc_genre = ' ';
+}
+
+Node_items_collection::Node_items_collection(string id, string title, string type, string loan_type, string copies, string fee) {
+    this->node_itc_id = id;
+    this->node_itc_title = title;
+    this->node_itc_types = type;
+    this->node_itc_loans_types = loan_type;
+    this->node_itc_copies = copies;
+    this->node_itc_fees = fee;
+}
+
+Node_items_collection::Node_items_collection(string id, string title, string type, string loan_type, string copies, string fee, string genre) {
+    this->node_itc_id = id;
+    this->node_itc_title = title;
+    this->node_itc_types = type;
+    this->node_itc_loans_types = loan_type;
+    this->node_itc_copies = copies;
+    this->node_itc_fees = fee;
+    this->node_itc_genre = genre;
+}
+
+string Node_items_collection::getID() {
+    return this->node_itc_id;
+}
+string Node_items_collection::getTitle() {
+    return this->node_itc_title;
+}
+string Node_items_collection::getType() {
+    return this->node_itc_types;
+}
+string Node_items_collection::getLoantype() {
+    return this->node_itc_loans_types;
+}
+string Node_items_collection::getCopies() {
+    return this->node_itc_copies;
+}
+string Node_items_collection::getFee() {
+    return this->node_itc_fees;
+}
+string Node_items_collection::getGenre() {
+    return this->node_itc_genre;
+}
+
+void Node_items_collection::setId(string ID) {
+    this->node_itc_id = ID;
+}
+void Node_items_collection::setTitle(string Title) {
+    this->node_itc_title = Title;
+}
+void Node_items_collection::setType(string Type) {
+    this->node_itc_types = Type;
+}
+void Node_items_collection::setLoanType(string loan_type) {
+    this->node_itc_loans_types = loan_type;
+}
+void Node_items_collection::setCopies(string copies) {
+    this->node_itc_copies = copies;
+}
+void Node_items_collection::setFee(string fee) {
+    this->node_itc_fees = fee;
+}
+void Node_items_collection::setGenre(string Genre) {
+    this->node_itc_genre = Genre;
+}
+void Node_items_collection::setAll(string ID, string Title, string Type, string loan_type, string copies, string fee, string genre) {
+    this->node_itc_id = ID;
+    this->node_itc_title = Title;
+    this->node_itc_types = Type;
+    this->node_itc_loans_types = loan_type;
+    this->node_itc_copies = copies;
+    this->node_itc_fees = fee;
+    this->node_itc_genre = genre;
+}
--- ADT/ItemsList.h
+++ ADT/ItemsList.h
@@ -12,7 +12,7 @@ using namespace std;
 class items_collection {
 public:
     string itc_id;
-    string itc_name;
+    string itc_title;
     string itc_types;
     string itc_loans_types;
     string itc_copies;
@@ -23,7 +23,7 @@ public:
 class Node_items_collection {
 public:
     string node_itc_id;
-    string node_itc_name;
+    string node_itc_title;
     string node_itc_types;
     string node_itc_loans_types;
     string node_itc_copies;
@@ -33,7 +33,7 @@ public:
 
     explicit Node_items_collection(items_collection &it) {
         this->node_itc_id = it.itc_id;
-        this->node_itc_name = it.itc_name;
+        this->node_itc_title = it.itc_title;
         this->node_itc_types = it.itc_types;
         this->node_itc_loans_types = it.itc_loans_types;
         this->node_itc_copies = it.itc_copies;
@@ -41,6 +41,28 @@ public:
         this->node_itc_genre = it.itc_genre;
     }
 
+    Node_items_collection();
+    Node_items_collection(string id, string title, string type, string loan_type, string copies, string fee);
+    Node_items_collection(string id, string title, string type, string loan_type, string copies, string fee, string genre);
+    Node_items_collection(Node_items_collection& item);
+    //Getter
+    string getID();
+    string getTitle();
+    string getType();
+    string getLoantype();
+    string getCopies();
+    string getFee();
+    string getGenre();
+
+    //Setter
+    void setId(string ID);
+    void setTitle(string Title);
+    void setType(string Type);
+    void setLoanType(string loan_type);
+    void setCopies(string copies);
+    void setFee(string fee);
+    void setGenre(string Genre);
+    void setAll(string ID, string Title, string Type, string loan_type, string copies, string fee, string genre);
 };
 
 
